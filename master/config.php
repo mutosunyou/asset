73dc07d5f8ad345e5881c1f3396b0417bdc364ed
mutosunyou <muto@muto.local>
2017-05-10 13:23:11 +0900

    copy

--- master/config.php
+++ master/config.php
@@ -3,12 +3,12 @@
 define('DB_HOST', "localhost");
 define('DB_USER', "root");
 define('DB_PASSWORD', "root");
-define('DB_NAME', "circular2");
+define('DB_NAME', "asset");
 
 //ini_set("display_errors", Off);
 error_reporting(0);//本番環境用
 
-define('SITE_URL', 'http://'.$_SERVER["SERVER_NAME"].'/circular2/');
+define('SITE_URL', 'http://'.$_SERVER["SERVER_NAME"].'/asset/');
 
 //不適合内容一覧のコンテンツプレビューの字数
 $previewlength = 150;
